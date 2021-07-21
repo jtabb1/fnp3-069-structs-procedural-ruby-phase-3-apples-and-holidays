34ead8b6dbdfdd410001a9d5b15d7c86a13e63f1
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-21 13:48:39 -0400

    Pass 7 of 8 tests.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,33 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each{|ky,vu| vu << supply}
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  # extra = {holiday_name => supply_array}
+  holiday_hash[season] = {holiday_name => supply_array}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -67,12 +69,38 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  out = []
+  holiday_hash.each do |ky1, vu1|
+    vu1.each do |ky2, vu2|
+      out << ky2 if vu2.include? "BBQ"
+    end
+  end
+  out
 end
 
+x = {
+  :winter => {
+    :christmas => ["Lights", "Wreath"],
+    :new_years => ["Party Hats"]
+  },
+  :summer => {
+    :fourth_of_july => ["Fireworks", "BBQ"]
+  },
+  :fall => {
+    :thanksgiving => ["Turkey"]
+  },
+  :spring => {
+    :memorial_day => ["BBQ"]
+  }
+}
 
+columbus_day_supplies = ["Flags", "Parade Floats", "Italian Food"]
 
+y = add_new_holiday_with_supplies(x, :fall, :columbus_day, columbus_day_supplies)
 
+# binding.pry
 
+columbus_day_supplies = ["Flags", "Parade Floats", "Italian Food"]
+c = columbus_day_supplies
 
 
